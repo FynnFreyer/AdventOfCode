0341836c217e3b52a22634f544064cb9e2656b3a
Fynn Freyer <fynn.freyer@googlemail.com>
2023-12-13 23:22:52 +0100

    feat: solve part two of 03.12.

--- 03/advent03.py
+++ 03/advent03.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from math import prod
 from pathlib import Path
 from re import compile
 from typing import Self, Optional
 
 SYMBOL = compile(r"[^\d.\n]")
 NUMBER = compile(r"(\d+)")
+GEAR = compile(r"(\*)")
 
 
 def surroundings(row: int, col: int, max_rows: int = 10, max_cols: int = 10) -> tuple[tuple[int, int], ...]:
@@ -50,11 +52,19 @@ class PartNumber:
             matches = SYMBOL.finditer(line)
             for match in matches:
                 symbol_col, _ = match.span()
-                neighborhood = surroundings(symbol_row, symbol_col, max_rows=max_rows, max_cols=max_cols)
-                for row, col in neighborhood:
-                    part_number = cls.from_pos(schematic, row, col)
-                    if part_number is not None:
-                        part_numbers.add(part_number)
+                neighboring_parts = cls.from_neighborhood(schematic, symbol_row, symbol_col,
+                                                          max_rows=max_rows, max_cols=max_cols)
+                part_numbers.update(neighboring_parts)
+        return part_numbers
+
+    @classmethod
+    def from_neighborhood(cls, schematic: str, row: int, col: int, max_rows: int = 10, max_cols: int = 10):
+        part_numbers = set()
+        neighborhood = surroundings(row, col, max_rows=max_rows, max_cols=max_cols)
+        for row, col in neighborhood:
+            part_number = cls.from_pos(schematic, row, col)
+            if part_number is not None:
+                part_numbers.add(part_number)
         return part_numbers
 
 
@@ -65,6 +75,33 @@ def parse_file_part_one(file: str | Path) -> int:
     return sum(number.value for number in PartNumber.from_schematic(schematic))
 
 
+def get_gear_ratios(schematic: str) -> list[int]:
+    lines = schematic.splitlines()
+
+    max_rows = len(lines)
+    max_cols = len(lines[0])
+
+    gear_ratios = []
+    for gear_row, line in enumerate(lines):
+        gears = GEAR.finditer(line)
+        for gear in gears:
+            gear_col, _ = gear.span()
+            neighboring_parts = PartNumber.from_neighborhood(schematic, gear_row, gear_col,
+                                                             max_rows=max_rows, max_cols=max_cols)
+            if len(neighboring_parts) == 2:
+                ratio = prod(part.value for part in neighboring_parts)
+                gear_ratios.append(ratio)
+
+    return gear_ratios
+
+
+def parse_file_part_two(file: str | Path) -> int:
+    with open(file) as file:
+        schematic = file.read()
+
+    return sum(ratio for ratio in get_gear_ratios(schematic))
+
+
 if __name__ == '__main__':
-    sum_of_part_nos = parse_file_part_one("input.txt")
-    print(sum_of_part_nos)
+    sum_of_ratios = parse_file_part_two("input.txt")
+    print(sum_of_ratios)
